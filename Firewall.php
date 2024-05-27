b30ea89392a5dc2a6284cf74fe4e2bcff9d483d5
David <55126143+devdavede@users.noreply.github.com>
2024-05-27 16:07:27 +0200

    Update Firewall.php
    Changed variables for constants

--- Firewall.php
+++ Firewall.php
@@ -3,8 +3,8 @@ include("_database.php");
 
 class Firewall {
     private $database;
-    private $blocklist = "blocked_ips.txt";
-    private $blockAfterAttempts = 3;
+    private const BLOCKLIST = "blocked_ips.txt";
+    private const BLOCK_AFTER_ATTEMPTS = 3;
     private const MALICIOUS_FILES = ["repeater.php", "admin.php", "inc.php", "lv.php", "seo.php", "x.php", "b0x.php", "about.php", "cloud.php", "wso.php", "cong.php", "network.php", "css.php", "wp-2019.php", "atomlib.php", "css.php", "simple.php", "log.php", "index.php", "mail.php", "lufix.php", "doc.php", "bak.php", "content.php", "upfile.php", "wp.php", "wp-conflg.php", "bypass.php", "wp-22.php", "wp-activate.php", "404.php", "updates.php", "radio.php", "plugins.php", "xmrlpc.php", "ae.php", "moon.php", "blog.php", "themes.php", "ini.php", "as.php", "shell.php", "ws.php", "dropdown.php", "makeasmtp.php", "wp-sigunq.php", "wso112233.php", "alfanew.php", "fw.php", "install.php", "wp-login.php", "mini.php", "configs.php", "test.php", "classsmtps.php", "wp-hudbud.php", "inputs.php", "autoload_classmap.php", "text.php"];
 
     function __construct() {
@@ -13,7 +13,7 @@ class Firewall {
     }
 
     function blockIP($ip) {
-        $blacklist_content = file_get_contents($this->blocklist);
+        $blacklist_content = file_get_contents(SELF::BLOCKLIST);
 
         if(str_contains($blacklist_content, $ip)){
             return;
@@ -34,7 +34,7 @@ class Firewall {
 
         $entries = $this->database->Query("SELECT * FROM accesslog WHERE ip = ? AND code = 404 AND created_at >= NOW() - INTERVAL 5 MINUTE", [$ip]);
 
-        if(count($entries) > $this->blockAfterAttempts) {
+        if(count($entries) > SELF::BLOCK_AFTER_ATTEMPTS) {
             $this->blockIP($ip);
         }
         
